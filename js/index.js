513d3b70e6f040d2dac1a0ac8d463c8d14fab302
Eliabe Gai <101521522+Eliabegai@users.noreply.github.com>
2023-03-25 12:30:23 -0300

    fixed, feito ógica para todos os pokemons

--- js/index.js
+++ js/index.js
@@ -3,35 +3,66 @@ const pokemonNumber = document.querySelector('.pokemon-number')
 const pokemonImage = document.querySelector('.pokemon-image')
 const form = document.querySelector('.form')
 const input = document.querySelector('.input-search')
+const buttonPrev = document.querySelector('.btn-prev')
+const buttonNext = document.querySelector('.btn-next')
 
+let searchPokemon = 1
 
 const fetchPokemon = async (pokemon) => {
 
-  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`)
-
-  const data = await APIResponse.json()
-
-  return data
+  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
+  if (APIResponse.status === 200) {
+    const data = await APIResponse.json()
+    return data
+  }
 }
-
 
 const renderPokemon = async (pokemon) => {
 
+  pokemonName.innerHTML = 'Loading ...'
+  pokemonNumber.innerHTML = ''
+
   const data = await fetchPokemon(pokemon)
 
-  pokemonName.innerHTML = data.name
-  pokemonNumber.innerHTML = data.id
-  pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+  if (data) {
+    pokemonImage.style.display = 'block'
+    pokemonName.innerHTML = data.name
+    pokemonNumber.innerHTML = data.id
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'] || data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'] || data['sprites']['versions']['generation-v']['black-white']['front_default'] ||
+    data['sprites']['front_default'] || data['sprites']['other']['official-artwork']['front_default']
 
-  input.value = ''
-
-
+    input.value = ''
+    searchPokemon = data.id
+  } else {
+    pokemonImage.style.display = 'none'
+    pokemonName.innerHTML = 'Not found :c'
+    pokemonNumber.innerHTML = ''
+  }
 }
-
 
 form.addEventListener('submit', (event) => {
   event.preventDefault()
-
-  renderPokemon(input.value)
-
+  renderPokemon(input.value.toLowerCase())
 })
+
+buttonPrev.addEventListener('click', (event) => {
+  if (searchPokemon > 1) {
+    searchPokemon-= 1
+    renderPokemon(searchPokemon)
+  } else {
+    searchPokemon = 1010
+    renderPokemon(searchPokemon)
+  }
+})
+
+buttonNext.addEventListener('click', (event) => {
+  if (searchPokemon === 1010) {
+    searchPokemon = 1
+    renderPokemon(searchPokemon)
+  } else {
+    searchPokemon += 1
+    renderPokemon(searchPokemon)
+  }
+})
+
+renderPokemon(searchPokemon)
